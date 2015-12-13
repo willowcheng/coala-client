025d2db512422211fd843cfd84710138268f18bf
willowcheng <e.wingus@Gmail.com>
2015-12-12 19:50:02 -0500

    Initial Commit

---
+++ Coala/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Coala
+//
+//  Created by Liu Cheng on 2015-12-12.
+//  Copyright © 2015 Liu Cheng. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
